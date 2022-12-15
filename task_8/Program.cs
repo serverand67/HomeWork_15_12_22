3292f2c1ab59e2c9e49dcc1fde02ea6079f50c92
unknown <veronika040182@hotmail.com>
2022-12-16 00:03:41 +0300

    задача 8

---
+++ task_8/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 8. Напишите программу, которая на вход принимает число N, а на выходе показывает
+ве четные числа от 1 до N */
+
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+int temp = 2;
+
+while(temp <= N)
+{
+Console.WriteLine(temp);
+temp = temp + 2;
+}
